8327f4e48cd8c6cda1ad37f4401c3e3da139010d
osamatoma <osamahassan333333@hotmail.com>
2018-11-23 06:45:58 +0300

    init commit

---
+++ resources/js/routes.js
@@ -0,0 +1,16 @@
+let routes = { 
+    '0': 'http://projectflyer.test/api/user', 
+    '1': 'http://projectflyer.test//', 
+    'login': 'http://projectflyer.test/login', 
+    '3': 'http://projectflyer.test/login', 
+    'logout': 'http://projectflyer.test/logout', 
+    'register': 'http://projectflyer.test/register', 
+    '6': 'http://projectflyer.test/register', 
+    'password.request': 'http://projectflyer.test/password/reset', 
+    'password.email': 'http://projectflyer.test/password/email', 
+    'password.reset': 'http://projectflyer.test/password/reset/{token}', 
+    'password.update': 'http://projectflyer.test/password/reset', 
+    'home': 'http://projectflyer.test/home', 
+
+ }; 
+ export default routes
